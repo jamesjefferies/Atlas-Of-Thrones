0547877e5c8a1bc0994c9845a82883944ddc41b6
James Jefferies <james@shedcode.co.uk>
2020-06-01 15:49:41 +0100

    Hello world

--- server/api.js
+++ server/api.js
@@ -1,3 +1,16 @@
 /**
  * API Routes Module
  */
+const Router = require('koa-router')
+const database = require('./database')
+const cache = require('./cache')
+const joi = require('joi')
+const validate = require('koa-joi-validate')
+
+const router = new Router()
+
+router.get('/hello', async ctx => {
+  ctx.body = 'Hello World'
+})
+
+module.exports = router
