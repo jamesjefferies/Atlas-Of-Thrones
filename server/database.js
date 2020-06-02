a37c7f3ab5a0b50de1eab9c62742f2c5afa6ccef
James Jefferies <james@shedcode.co.uk>
2020-06-02 20:24:25 +0100

    Working

--- server/database.js
+++ server/database.js
@@ -1,3 +1,14 @@
 /**
  * Postgres DB Module
  */
+const postgres = require('pg')
+const log = require('./logger')
+const connectionString = process.env.DATABASE_URL
+
+// Initialize postgres client
+const client = new postgres.Client({ connectionString: connectionString })
+
+// Connect to the DB
+client.connect().then(() => {
+  log.info(`Connected To ${client.database} at ${client.host}:${client.port}`)
+}).catch(log.error);
